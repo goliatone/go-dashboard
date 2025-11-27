996893d5a793e3f541f440b6b43a7f485d8dd2bc
goliatone <140582+goliatone@users.noreply.github.com>
2025-11-26 18:17:38 -0800

    add: theme support to dashboard

--- pkg/dashboard/dashboard.go
+++ pkg/dashboard/dashboard.go
@@ -10,6 +10,21 @@ type Service = core.Service
 // Options re-export for convenience.
 type Options = core.Options
 
+// ThemeProvider re-export for convenience.
+type ThemeProvider = core.ThemeProvider
+
+// ThemeSelector re-export for convenience.
+type ThemeSelector = core.ThemeSelector
+
+// ThemeSelectorFunc re-export for convenience.
+type ThemeSelectorFunc = core.ThemeSelectorFunc
+
+// ThemeSelection re-export for convenience.
+type ThemeSelection = core.ThemeSelection
+
+// ThemeAssets re-export for convenience.
+type ThemeAssets = core.ThemeAssets
+
 // NewService proxies to the internal constructor.
 func NewService(opts Options) *Service {
 	return core.NewService(opts)
